f937ba6a311b5062fccb059c9091834a0645a4f5
Bohdan Tolmachov <bogdantolmachevvv@gmail.com>
2024-08-19 21:32:10 +0200

    update root package.json

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import clicksRoute from './src/routes/click.routes.js';
 import predictionRoutes from "./src/routes/prediction.routes.js";
 import dotenv from 'dotenv';
 import cors from 'cors';
+import * as path from "node:path";
 
 dotenv.config();
 const app = express();
@@ -19,6 +20,10 @@ app.use('/api/predictions', predictionRoutes);
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Could not connect to MongoDB...', err));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend/dist', 'index.html'));
+});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running on port ${process.env.PORT || 3000}`);
